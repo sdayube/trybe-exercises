95c8f181719fca58c409e17474d1a01a0dbb114d
Silvio Dayube <scarige@gmail.com>
2020-10-23 15:33:08 -0300

    Adds first set of exercises from W3 schools

---
+++ 3. HTML and CSS/4. Javascript/4.1. JS First Steps/mini-exercises.js
@@ -0,0 +1,21 @@
+// Create a variable called carName, assign the value Volvo to it.
+var carName = "Volvo";
+
+// Create a variable called x, assign the value 50 to it.
+var x = 50;
+
+// Display the sum of 5 + 10, using two variables: x and y.
+var x = 5;
+var y = 10;
+console.log(x + y);
+
+// Create a variable called z, assign x + y to it, and display the result.
+var z = x + y;
+console.log(z);
+
+// On one single line, declare three variables with the following names and values:
+
+// firstName = "John"
+// lastName = "Doe"
+// age = 35
+var firstName = "John", lastName = "Doe", age = 35;
