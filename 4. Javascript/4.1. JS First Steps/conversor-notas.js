952bc4c49e1025cd6bdfbe19137d7a7a8f636a45
Silvio Dayube <scarige@gmail.com>
2020-10-23 19:10:28 -0300

    Adds grade converter

---
+++ 4. Javascript/4.1. JS First Steps/conversor-notas.js
@@ -0,0 +1,17 @@
+let a = 100;
+
+if (a > 100 || a < 0) {
+  console.log("erro - nota deve estar entre 100 e 0");
+} else if (a >= 90) {
+  console.log("A");
+} else if (a >= 80) {
+  console.log("B");
+} else if (a >= 70) {
+  console.log("C");
+} else if (a >= 60) {
+  console.log("D");
+} else if (a >= 50) {
+  console.log("E");
+} else {
+  console.log("F")
+}
