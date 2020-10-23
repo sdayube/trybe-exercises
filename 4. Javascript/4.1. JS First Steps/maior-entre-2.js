4ef7f6467e8b5c08d235aee6e33f92faef58a196
Silvio Dayube <scarige@gmail.com>
2020-10-23 18:53:28 -0300

    Adds ; to statements

--- 4. Javascript/4.1. JS First Steps/maior-entre-2.js
+++ 4. Javascript/4.1. JS First Steps/maior-entre-2.js
@@ -2,9 +2,9 @@ let a = 10;
 let b = 200;
 
 if (a > b) {
-  console.log(a)
+  console.log(a);
 } else if (a < b) {
-  console.log(b)
+  console.log(b);
 } else {
   console.log("Numbers are equal");
 }
--- 4. Javascript/4.1. JS First Steps/maior-entre-3.js
+++ 4. Javascript/4.1. JS First Steps/maior-entre-3.js
@@ -3,11 +3,11 @@ let b = 11;
 let c = 11;
 
 if (a > b && a > c) {
-  console.log(a)
+  console.log(a);
 } else if (b > a && b > c) {
-  console.log(b)
+  console.log(b);
 } else if (c > a && c > b) {
-  console.log(c)
+  console.log(c);
 } else {
   console.log("There isn't a single higher number");
 }
