8cecb24d62894841f8e83520d4ab1bcb37a0e493
Silvio Dayube <scarige@gmail.com>
2020-10-23 23:30:05 -0300

    Considers zero as a pair

--- 4. Javascript/4.1. JS First Steps/8_par.js
+++ 4. Javascript/4.1. JS First Steps/8_par.js
@@ -2,7 +2,7 @@ let a = 11;
 let b = 11;
 let c = 11;
 
-if (a % 2 == 0 && a != 0 || b % 2 == 0 && b != 0 || c % 2 == 0 && a != 0) {
+if (a % 2 == 0 || b % 2 == 0 || c % 2 == 0) {
   console.log("true");
 } else {
   console.log("false");
