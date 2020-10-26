3e4384206bacb4a2e7f0391d904bf14f680e9e52
Silvio Dayube <scarige@gmail.com>
2020-10-26 19:12:51 -0300

    Adds exercise 5 - Find highest number within array

--- 4. Javascript/4.2. Array and For Loop/4_twenty_comparison.js
+++ 4. Javascript/4.2. Array and For Loop/4_twenty_comparison.js
@@ -1,6 +1,6 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-// Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
+// Com o mesmo código do exercício anterior, caso valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
 
 let numberSum = 0;
 
---
+++ 4. Javascript/4.2. Array and For Loop/5_for_highest.js
@@ -0,0 +1,13 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// Utilizando for, descubra qual o maior valor contido no array e imprima-o;
+
+let numberHighest = 0;
+
+for (let i = 0; i < numbers.length; i += 1) {
+  if (numberHighest < numbers[i]) {
+    numberHighest = numbers[i];    
+  }
+}
+
+console.log(numberHighest);
