39b93263ae8cf68c85317f78f5288110b6070003
Silvio Dayube <68237073+sdayube@users.noreply.github.com>
2020-10-27 13:58:00 -0300

    Corrects exercise 1 to use a for loop

--- 4. Javascript/4.2. Array and For Loop/1_array_elements.js
+++ 4. Javascript/4.2. Array and For Loop/1_array_elements.js
@@ -3,4 +3,6 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 // -------------------
 // Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log();
 
-console.log(numbers);
+for (let i = 0; i < numbers.length; i += 1) {
+  console.log(numbers[i]);
+}
