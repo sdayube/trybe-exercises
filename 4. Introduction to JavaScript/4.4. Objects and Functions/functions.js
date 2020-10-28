19682c326d977ff4f5059ec1d20ba79d01cd2340
Silvio Dayube <scarige@gmail.com>
2020-10-28 16:46:55 -0300

    Adds last functions to 4.4 functions.js

--- 4. Introduction to JavaScript/4.4. Objects and Functions/functions.js
+++ 4. Introduction to JavaScript/4.4. Objects and Functions/functions.js
@@ -66,7 +66,7 @@ function chessBishop(a) {
   }
 }
 
-//Função 7 - Se a peça for bispo, qual o movimento?
+//Função 7 - Conversor de notas de 0 a 100 para A a F
 function gradeConvert(a) {
   if (a > 100 || a < 0) {
     return "erro - nota deve estar entre 100 e 0";
@@ -85,5 +85,73 @@ function gradeConvert(a) {
   }  
 }
 
+//Função 8 - Dentre três números, algum é par?
+function evenChecker(a, b, c) {
+  if (a % 2 == 0 || b % 2 == 0 || c % 2 == 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
-console.log(gradeConvert(23));
+//Função 9 - Dentre três números, algum é ímpar?
+function unevenChecker(a, b, c) {
+  if (a % 2 != 0 || b % 2 != 0 || c % 2 != 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+//Função 10 - Lucro/prejuízo da venda de 1000 produtos
+function profit(cost, sell) {
+  let totalCost = (cost * 1.2);
+  if (cost < 0 || sell < 0) {
+    return "Erro: os valores não podem ser negativos";
+  } else {
+    return (sell - totalCost) * 1000;
+  }
+}
+
+//Função 11 - Cálculo de salário líquido
+function salarioLiquido(salarioBruto) {
+  let valorInss, salarioBase, valorImposto, abateImposto;
+
+  if (salarioBruto <= 1556.94) {
+    valorInss = salarioBruto * 0.08;
+  } else if (salarioBruto <= 2594.92) {
+    valorInss = salarioBruto * 0.09;
+  } else if (salarioBruto <= 5189.82) {
+    valorInss = salarioBruto * 0.11;
+  } else {
+    valorInss = 570.88;
+  }
+
+  salarioBase = (salarioBruto - valorInss);
+
+  if (salarioBase <= 1903.98) {
+    valorImposto = 0;
+    abateImposto = 0;
+  } else if (salarioBase <= 2826.65) {
+    valorImposto = salarioBase * 0.075;
+    abateImposto = 142.80;
+  } else if (salarioBase <= 3751.05) {
+    valorImposto = salarioBase * 0.15;
+    abateImposto = 354.80;
+  } else if (salarioBase <= 4664,68) {
+    valorImposto = salarioBase * 0.225;
+    abateImposto = 636.13;
+  } else {
+    valorImposto = salarioBase * 0.275;
+    abateImposto = 869.36;
+  }
+
+  return `  Salário bruto = ${salarioBruto};
+  INSS = ${valorInss};
+  Salário base = ${salarioBase};
+  IR = ${valorImposto - abateImposto};
+  Salário líquido = ${salarioBase - valorImposto + abateImposto}.`;
+}
+
+
+console.log(salarioLiquido(3000));
