dedab20ea5626386a3d58e4dd2ac2423fb85913f
Silvio Dayube <scarige@gmail.com>
2020-11-04 20:06:43 -0300

    Adds exercise 1 of 5.3

--- 5. JavaScript: DOM, Events and Web Stages/5.3. Events/script.js
+++ 5. JavaScript: DOM, Events and Web Stages/5.3. Events/script.js
@@ -14,3 +14,30 @@ function createDaysOfTheWeek() {
 createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
+
+// Exercício 1
+const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+function dayPopulator() {
+  let daysUL = document.getElementById('days');
+
+  for (let index in dezDaysList) {
+    let day = dezDaysList[index];
+    let listItem = document.createElement('li')
+
+    listItem.innerText = day;
+    listItem.className = 'day';
+
+    if (day === 24 || day === 25 || day === 31) {
+      listItem.className = listItem.className + ' holiday';
+    }
+
+    if (day === 4 || day === 11 || day === 18 || day === 25) {
+      listItem.className = listItem.className + ' friday';
+    }
+
+    daysUL.appendChild(listItem);
+  }
+}
+
+dayPopulator();
