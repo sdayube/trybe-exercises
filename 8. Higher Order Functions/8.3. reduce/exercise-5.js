513750c45ba98f61ff7b33bdb7c3051cefc0509c
Silvio Dayube <scarige@gmail.com>
2020-12-02 18:20:07 -0300

    Adds exercise 5 to 8.3

---
+++ 8. Higher Order Functions/8.3. reduce/exercise-5.js
@@ -0,0 +1,13 @@
+const assert = require('assert');
+
+const names = [
+  'Aanemarie', 'Adervandes', 'Akifusa',
+  'Abegildo', 'Adicellia', 'Aladonata',
+  'Abeladerco', 'Adieidy', 'Alarucha',
+];
+
+function containsA() {
+  return names.reduce((acc, val) => `${acc}${val}`).match(/a/gi).length;
+}
+
+assert.deepStrictEqual(containsA(), 20);
